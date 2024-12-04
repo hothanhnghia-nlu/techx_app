cd38194b1f345964319c79658f2b33c5e62cdc58
minhdat20130226 <20130226@st.hcmuaf.edu.vn>
2024-12-04 21:55:45 +0700

    Complete reviews functionality

---
+++ lib/utils/date_format .dart
@@ -0,0 +1,12 @@
+import 'package:intl/intl.dart';
+
+String formatDateTime(String isoString) {
+  try {
+    DateTime dateTime = DateTime.parse(isoString);
+    String formattedDate = DateFormat('dd/MM/yyyy HH:mm:ss').format(dateTime);
+    
+    return formattedDate;
+  } catch (e) {
+    return 'Invalid date';
+  }
+}
